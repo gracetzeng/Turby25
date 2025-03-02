ce622bbee28d158b418551c9451cb0f1fb36aae1
grace <zengt.grace@gmail.com>
2025-03-02 09:49:47 -0600

    Update camera_opencv.py

--- src/camera_opencv.py
+++ src/camera_opencv.py
@@ -3,12 +3,14 @@ import numpy as np
 
 class CameraProcessor:
     def __init__(self):
+        """Initializes the camera for capturing video."""
         self.cap = cv2.VideoCapture(0)  # Use the default camera
         if not self.cap.isOpened():
             print("Error: Could not open camera.")
             exit()
 
     def get_frame(self):
+        """Captures a frame from the camera."""
         ret, frame = self.cap.read()
         if not ret:
             return None
@@ -19,9 +21,16 @@ class CameraProcessor:
         # Convert the frame from BGR to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Define the range of blue color in HSV
+        # Define the range of colors
         lower_blue = np.array([100, 150, 50])  # Lower bound of blue
         upper_blue = np.array([140, 255, 255])  # Upper bound of blue
+        
+        upper_red = np.array([255, 171, 171])  # Lower bound of red
+        lower_red = np.array([163, 33, 33])  # Upper bound of red
+        
+
+        upper_green = np.array([168, 201, 119])  # Lower bound of green
+        lower_green = np.array([117, 191, 6])  # Upper bound of green
 
         # Threshold the HSV image to get only blue colors
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
